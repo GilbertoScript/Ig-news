2ad41281c47bc11542ae670e762225e0e46f5da7
GilbertoASJ <alvesgilberto84@gmail.com>
2021-04-02 17:46:38 -0300

    :sparkles: Updating files

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+// Importações
+import styles from './styles.module.scss'
+
+// Como o header irá ficar em todas as páginas, colocamos ele no _app.tsx
+export function Header() {
+	
+	return (
+		<header className={styles.headerContainer}>
+			<div className={styles.headerContent}>
+				<h1>Ig.news</h1>
+				<nav>
+					<a className={styles.active} href="#">Home</a>
+					<a href="#">Post</a>
+				</nav>
+			</div>
+		</header>
+	)
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
 				<Head>
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
+					<link rel="shortcut icon" href="/favicon.png" />
 				</Head>
 				<body>
 					
