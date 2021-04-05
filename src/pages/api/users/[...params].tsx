e84ca223d447751e327adc8df6a6f874b6ea6519
GilbertoASJ <alvesgilberto84@gmail.com>
2021-04-05 17:34:40 -0300

    :sparkles: Creating api/users folder

---
+++ src/pages/api/users/[...params].tsx
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+// Estratégia de autenticação => JWT (Storage), Next Auth (Social), Cognito, Auth0
+ 
+export default (request: NextApiRequest, response: NextApiResponse) => {
+
+	console.log(request.query)
+
+	const users = [
+		{ id: 1, name: 'Fulano' },
+		{ id: 2, name: 'Ciclano' },
+		{ id: 3, name: 'Beltrano' },
+	]
+
+	return response.json(users)
+}
--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,4 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+
+// Estratégia de autenticação => JWT (Storage), Next Auth (Social), Cognito, Auth0
  
 export default (request: NextApiRequest, response: NextApiResponse) => {
 
