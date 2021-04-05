03055b85cc92ff2e0ccd800aee0b0501e3b2167b
GilbertoASJ <alvesgilberto84@gmail.com>
2021-04-05 16:36:38 -0300

    :sparkles: Update file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,11 @@
 import Head from 'next/head'
 import styles from './home.module.scss'
 
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import { SubscribeButton } from '../components/SubscribeButton'
 import { stripe } from '../services/stripe';
+
+// Formas de se fazer chamada a api com next: Client-side, Server-side, Static site generation
 
 interface HomeProps {
   product: {
@@ -38,7 +40,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   const price = await stripe.prices.retrieve('price_1IcaWjBYLKh1ePkAvAQgcLGl')
 
@@ -53,6 +55,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 Horas
   }
 }
