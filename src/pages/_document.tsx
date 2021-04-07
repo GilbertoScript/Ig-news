50fe93428c497fbe9da39f3ef71ef569ba1feab7
GilbertoASJ <alvesgilberto84@gmail.com>
2021-04-07 17:17:31 -0300

    :sparkles: Linking favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
 				<Head>
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
-					<link rel="shortcut icon" href="/favicon.png" />
+					<link rel="shortcut icon" href="https://cdn.iconscout.com/icon/free/png-256/react-1-282599.png" />
 				</Head>
 				<body>
 					
