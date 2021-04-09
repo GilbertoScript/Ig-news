0c52aa28b208b31080ae7ebfeb0897f3752fd149
GilbertoASJ <alvesgilberto84@gmail.com>
2021-04-09 16:41:40 -0300

    :sparkles: Updating project

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -21,10 +21,28 @@ export default NextAuth({
 
   		try {
   			await fauna.query(
-  				q.Create(
-  					q.Collections('users'),
-  					{ data: { email } }
-  				)
+  				q.If(
+             q.Not(
+               q.Exists(
+                  q.Match(
+                     q.Index('user_by_email'),
+                     q.Casefold(user.email)
+                  )
+               )
+             ),
+
+             q.Create(
+                 q.Collection('users'),
+                 { data: { email } }
+              ),
+
+             q.Get(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+             )
+          )
   			)
 
   			return true
