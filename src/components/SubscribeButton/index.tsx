b00c46e90111f364b99d083d1a60fe53b37f923c
GilbertoASJ <alvesgilberto84@gmail.com>
2021-04-09 17:54:53 -0300

    :sparkles: Updating file

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,13 +1,28 @@
 import styles from './styles.module.scss'
 
+import { useSession, signIn } from 'next-auth/client'
+
 interface SubscribeButtonProps {
 	priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+
+	const [session] = useSession();
+
+	function handleSubscribe() {
+		if(!session) {
+
+			signIn('github')
+			return
+		}
+
+		// Criação da checkout session
+
+	}
 	
 	return (
-		<button type="button" className={styles.subscribeButton}>
+		<button type="button" className={styles.subscribeButton} onClick={handleSubscribe}>
 			Subscribe now
 		</button>
 	)
