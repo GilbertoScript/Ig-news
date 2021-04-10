0c914b0b8632ee08e691b570948df0e751d15f16
GilbertoASJ <alvesgilberto84@gmail.com>
2021-04-10 16:40:10 -0300

    :sparkles: Updating file

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,8 @@
 import styles from './styles.module.scss'
 
 import { useSession, signIn } from 'next-auth/client'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
 
 interface SubscribeButtonProps {
 	priceId: string;
@@ -10,14 +12,25 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
 
 	const [session] = useSession();
 
-	function handleSubscribe() {
+	async function handleSubscribe() {
 		if(!session) {
 
 			signIn('github')
 			return
 		}
 
-		// Criação da checkout session
+		try {
+			const response = await api.post('/subscribe')
+
+			const { sessionId } = response.data
+
+			const stripe = await getStripeJs()
+
+			await stripe.redirectToCheckout({ sessionId })
+
+		} catch(err) {
+			alert(err.message)
+		}
 
 	}
 	
