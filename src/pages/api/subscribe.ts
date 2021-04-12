2fec618af6e1e1d8d60056c844a465212a291b4b
GilbertoASJ <alvesgilberto84@gmail.com>
2021-04-12 15:29:34 -0300

    :sparkles: Updating file

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -32,25 +32,30 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
 		let customerId = user.data.stripe_customer_id
 
-		const stripeCustomer = await stripe.customers.create({
-			email: session.user.email,
-			// metadata
-		})
+		if(!customerId) {
+			const stripeCustomer = await stripe.customers.create({
+				email: session.user.email,
+				// metadata
+			})
 
-
-		await fauna.query(
-			q.Update(
-				q.Ref(q.Collection('users'), user.ref.id),
-				{
-					data: {
-						stripe_customer_id: stripeCustomer.id,
+			await fauna.query(
+				q.Update(
+					q.Ref(q.Collection('users'), user.ref.id),
+					{
+						data: {
+							stripe_customer_id: stripeCustomer.id,
+						}
 					}
-				}
+				)
 			)
-		)
+
+			customerId = stripeCustomer.id
+		}
+
+		
 
 		const stripeCheckoutSession = await stripe.checkout.sessions.create({
-			customer: stripeCustomer.id,
+			customer: customerId,
 			payment_method_types: ['card'],
 			billing_address_collection: 'required',
 			line_items: [
