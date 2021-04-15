387eb70b5380ba798871c22683329d09d14154ee
GilbertoASJ <alvesgilberto84@gmail.com>
2021-04-15 17:19:18 -0300

    :sparkles: Updating file

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -1,3 +1,7 @@
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/faunadb'
+import { stripe } from '../../../services/stripe'
+
 export async function saveSubscription(
 	subscriptionId: string,
 	customerId: string,
@@ -6,5 +10,31 @@ export async function saveSubscription(
 	// Buscar o usuário no banco do fauna com o Id customerId
 	// Salvar os dados da subscription no Faunadb
 
-	console.log(subscriptionId, customerId)
+	const userRef = await fauna.query(
+		q.Select(
+			"ref",
+			q.Get(
+				q.Match(
+					q.Index('user_by_stripe_customer_id'),
+				customerId
+				)
+			)
+		)
+	)
+
+	const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+
+	const subscriptionData = {
+		id: subscription.id,
+		userId: userRef,
+		status: subscription.status,
+		price_id: subscription.items.data[0].price.id
+	}
+
+	await fauna.query(
+		q.Create(
+			q.Collection('subscriptions'),
+			{ data: subscription }
+		)
+	)
 }
